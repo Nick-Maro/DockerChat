652b87ec476869f5a989992ed8647146f15586d6
gkkconan <conan.gkk@gmail.com>
2025-08-18 13:35:40 +0200

    feat(client): add online badge in dm list

--- client/src/components/directMessages.tsx
+++ client/src/components/directMessages.tsx
@@ -12,6 +12,7 @@ export default function DirectMessages(){
             <ul className="flex column">
                 {clients.map((client, index) => { return (
                     <li className={styles.active} key={client.client_id}>
+                        {client.online ? <span className={styles.onlineBadge}></span> : undefined }
                         <img src={`https://avatar.iran.liara.run/public/girl?username=${client.client_id}`} alt="pfp" />
                         <div className={styles.info}>
                             <div className={`${styles.name} center-flex row`}>
