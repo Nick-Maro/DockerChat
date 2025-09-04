fd978ae6b26cec2c71f917a265d8fa642d536eef
gkkconan <conan.gkk@gmail.com>
2025-09-05 00:56:58 +0200

    fix(client): update avatar placeholder alignment

--- client/src/components/avatar.tsx
+++ client/src/components/avatar.tsx
@@ -5,9 +5,9 @@ export default function Avatar({ username }) {
   const [loaded, setLoaded] = useState(false);
 
   return (
-    <>
+    <div style={{ width: 50, height: 50 }}>
       {!loaded && ( <img src={placeholder} alt="placeholder" /> )}
-      <img src={`https://avatar.iran.liara.run/public?username=${username}`} alt="pfp" onLoad={() => setLoaded(true)} width={50} />
-    </>
+      <img src={`https://avatar.iran.liara.run/public?username=${username}`} onLoad={() => setLoaded(true)} width={50} />
+    </div>
   );
 }
