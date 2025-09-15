52e01ea548176b78d5f63224aedafaba2a44c333
nickmaro <nick007sbt@gmail.com>
2025-09-15 18:01:04 +0200

    fix(client): update avatar placeholder

---
+++ client/src/components/avatar.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import placeholder from "../assets/icons/pfp-placeholder.svg";
+export default function Avatar({ username }) {
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <>
+      {!loaded && ( <img src={placeholder} alt="placeholder" /> )}
+      <img src={`https://avatar.iran.liara.run/public?username=${username}`} alt="pfp" onLoad={() => setLoaded(true)} width={50} />
+    </>
+  );
+}
